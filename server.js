980a1bbb41115a4d981614fb7092d2338d083468
Poornesh Mangapuram <poornesh806@gmail.com>
2026-02-24 00:27:33 +0530

    first commit

--- server.js
+++ server.js
@@ -16,20 +16,39 @@ const adminRoutes = require('./routes/admin');
 const studentRoutes = require('./routes/student');
 
 const app = express();
+app.set('trust proxy', 1);
 
 // Connect Database
 connectDB();
 
 // Security Middleware
 app.use(helmet({ crossOriginEmbedderPolicy: false }));
+// app.use(cors({
+//   origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.CLIENT_URL
+];
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 
-app.set('trust proxy', 1);
+
 
 // Rate Limiting
 const limiter = rateLimit({
